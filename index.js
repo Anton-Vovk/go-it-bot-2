69db62c88493385e739a88621b30a72d959d9618
Anton-Vovk <mr.vovk.anton@gmail.com>
2021-03-21 23:48:30 +0200

    update

--- index.js
+++ index.js
@@ -76,13 +76,13 @@ for (let i = 0; i < fruits.length; i += 1) {
 }
 
 // ----------------------------20----------------------
-function calculateTotalPrice(order) {
-  let total = 0;
+// function calculateTotalPrice(order) {
+//   let total = 0;
 
-  for (let i = 0; i < order.length; i++) total += order[i];
-  return total;
-}
-console.log(calculateTotalPrice([12, 85, 37, 4])); // 138
+//   for (let i = 0; i < order.length; i++) total += order[i];
+//   return total;
+// }
+// console.log(calculateTotalPrice([12, 85, 37, 4])); // 138
 
 // ----------------------------21----------------------
 function findLongestWord(string) {
@@ -110,23 +110,113 @@ function createArrayOfNumbers(min, max) {
 // console.log(createArrayOfNumbers(14, 17)); // [14, 15, 16, 17]
 
 // ----------------------------23----------------------
-function filterArray(numbers, value) {
-  const array = [];
+// function filterArray(numbers, value) {
+//   const array = [];
 
-  for (let i = 0; i <= numbers.length; i += 1) {
-    if (numbers[i] > value) {
-      array.push(numbers[i]);
-    }
-  }
-  // console.log(array);
-}
-filterArray([1, 2, 3, 4, 5], 3); // [4, 5]
-filterArray([1, 2, 3, 4, 5], 5) // []
+//   for (let i = 0; i <= numbers.length; i += 1) {
+//     if (numbers[i] > value) {
+//       array.push(numbers[i]);
+//     }
+//   }
+//   // console.log(array);
+// }
+// filterArray([1, 2, 3, 4, 5], 3); // [4, 5]
+// filterArray([1, 2, 3, 4, 5], 5); // []
 
 // ----------------------------24----------------------
 function checkFruit(fruit) {
-  const fruits = ['яблоко', 'слива', 'груша', 'апельсин'];
+  const fruits = ["яблоко", "слива", "груша", "апельсин"];
   return fruits.includes(fruit);
 }
 
 // ----------------------------25----------------------
+function getCommonElements(array1, array2) {
+  const common = [];
+
+  for (let item of array1) {
+    if (array2.includes(item)) {
+      common.push(item);
+    }
+  }
+  return common;
+}
+console.log(getCommonElements([1, 2, 3], [2, 1, 17, 19])); // [1, 2])
+
+// ----------------------------26----------------------
+function calculateTotalPrice(order) {
+  let total = 0;
+  for (let number of order) {
+    total += number;
+  }
+  return total;
+}
+
+// ----------------------------27----------------------
+function filterArray(numbers, value) {
+  const filteredNumbers = [];
+
+  for (let number of numbers) {
+    if (number > value) {
+      filteredNumbers.push(number);
+    }
+  }
+  return filteredNumbers;
+}
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+
+// ----------------------------28----------------------
+const a = 3 % 3; //0
+const b = 4 % 3; //1
+const c = 11 % 4; //3
+const d = 12 % 7; //5
+const e = 8 % 3; //2
+
+// ----------------------------29----------------------
+function getEvenNumbers(start, end) {
+  const array = [];
+  for (let i = start; i <= end; i += 1) {
+    if (i % 2 === 0) {
+      array.push(i);
+    }
+  }
+  return array;
+}
+console.log(getEvenNumbers(6, 12)); // [6, 8, 10, 12];
+console.log(getEvenNumbers(7, 7)); // [];
+
+// ----------------------------30----------------------
+const start = 6;
+const end = 27;
+let number;
+
+for (let i = start; i < end; i += 1) {
+  if (i % 5 === 0) {
+    number = i;
+    break;
+  }
+} //5
+
+// ----------------------------31----------------------
+function findNumber(start, end, divisor) {
+  let number;
+
+  for (let i = start; i < end; i += 1) {
+    if (i % divisor === 0) {
+      number = i;
+      return number;
+    }
+  }
+}
+
+// ----------------------------32----------------------
+function includes(array, value) {
+  for (let arr of array) {
+    if (arr === value) return true;
+  }
+  return false;
+}
+
+console.log(includes([1, 2, 3, 4, 5], 3)); // true
+console.log(includes([1, 2, 3, 4, 5], 17)); // false
+
+
